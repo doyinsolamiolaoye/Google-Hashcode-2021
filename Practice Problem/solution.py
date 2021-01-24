74d9265917e2917f876107679bce55403462827b
Doyinsolami Olaoye <64823598+doyinsolamiolaoye@users.noreply.github.com>
2021-01-24 20:43:19 +0100

    Update solution.py

--- Practice Problem/solution.py
+++ Practice Problem/solution.py
@@ -1,5 +1,30 @@
 # Google Hashcode 2021 Practice Problem
 
-
+# function to process input file and extract values
 def file_processing():
     
+    return
+
+
+# function to accept input values and solve the problem
+def solve(input_data):
+    
+    return
+
+
+# function to write output values (solution) to file
+def write_file():
+    
+    
+
+# contains the functions to run
+def main():
+    input_data = file_processing()
+    answer = solve(input_data)
+    write_file()
+
+if __name__ == '__main__':
+    main()
+
+
+# Done!
