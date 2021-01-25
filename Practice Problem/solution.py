54ca85a03473b9a081b3cb14299fa33c063e3250
Doyinsolami Olaoye <64823598+doyinsolamiolaoye@users.noreply.github.com>
2021-01-25 11:54:12 +0100

    Update solution.py

--- Practice Problem/solution.py
+++ Practice Problem/solution.py
@@ -2,30 +2,39 @@
 
 # function to process input file and extract values
 def file_processing(file):
+    # this method is more efficient when working with big data
+    with open(file) as f:
+        opened_file = [line.strip().split() for line in f] #a list of list such that each line is a list
+        
+    first_line = opened_file[0] #assigns first line in the list of list to first_line variable
     
-
+    print("There are {} pizzas for {}".format(first_line[0], file))
+    print("{} team of two".format(first_line[1]))
+    print("{} team of three".format(first_line[2]))
+    print("{} team of four\n".format(first_line[3]))
+    
+    return opened_file #returns the list of list
 
 # function to accept input values and solve the problem
 def solve(input_data):
     
-    return
 
-
-# function to write output values (solution) to file
-def write_file():
-    
     pass
 
 # contains the functions to run
 def main():
-    for each_file in ["a_example", "b_little_bit_of_everything.in", "c_many_ingredients.in", "d_many_pizzas.in","e_many_teams.in"  ]:
+    for each_file in ["a_example", "b_little_bit_of_everything.in", "c_many_ingredients.in", "d_many_pizzas.in","e_many_teams.in"]:
         f = file_processing(each_file) # processes each file
+
         answer = solve(f) #solves each file
-        write_file(each_file, 'w') #writes each file with their dataset name
-        pass
-    
+
+        # writes each answer to a new file and save it with their dataset name in a text format
+        f = open(each_file + ".out", "w") 
+        f.write(str(answer))
+        f.close()
+
 if __name__ == '__main__':
     main()
-    
+
 
 # Done!
