c75b556ff328e514e88c06dfd629718cf11e6613
Doyinsolami Olaoye <64823598+doyinsolamiolaoye@users.noreply.github.com>
2021-01-25 12:01:42 +0100

    Update solution.py

--- Practice Problem/solution.py
+++ Practice Problem/solution.py
@@ -3,7 +3,7 @@
 # function to process input file and extract values
 def file_processing(file):
     # this method is more efficient when working with big data
-    with open(file) as f:
+    with open(file + ".in") as f:
         opened_file = [line.strip().split() for line in f] #a list of list such that each line is a list
         
     first_line = opened_file[0] #assigns first line in the list of list to first_line variable
@@ -22,13 +22,13 @@ def solve(input_data):
 
 # contains the functions to run
 def main():
-    for each_file in ["a_example", "b_little_bit_of_everything.in", "c_many_ingredients.in", "d_many_pizzas.in","e_many_teams.in"]:
+    for each_file in ["a_example", "b_little_bit_of_everything", "c_many_ingredients", "d_many_pizzas","e_many_teams"]:
         f = file_processing(each_file) # processes each file
 
         answer = solve(f) #solves each file
 
         # writes each answer to a new file and save it with their dataset name in a text format
-        f = open(each_file + ".out", "w") 
+        f = open("../solution/" + each_file + ".out", "w") 
         f.write(str(answer))
         f.close()
 
